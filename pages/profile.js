29d738a1b723a3f1fbeeab46920f9d1722f82078
FrameTee29 <teeraphat123@hotmail.com>
2020-01-12 16:23:53 +0700

    Can put in store v.2

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { increment, decrement, reset } from "../src/actions/counterActions";
 
 const profile=props=> {
   return (
@@ -17,12 +16,5 @@ const mapStateToProps = state => ({
   info: state.Profile.user
 });
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement()),
-    reset: () => dispatch(reset())
-  };
-};
 
-export default connect(mapStateToProps,mapDispatchToProps)(profile);
+export default connect(mapStateToProps)(profile);
