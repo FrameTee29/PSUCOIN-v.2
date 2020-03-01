c104f1894d1cca8a5d4908ffa02efc0528238766
FrameTee29 <teeraphat123@hotmail.com>
2020-03-01 17:03:23 +0700

    add contractAbi

--- src/components/transfer/subtransfer.js
+++ src/components/transfer/subtransfer.js
@@ -188,7 +188,81 @@ const subtransfer = props => {
 
                 var count = web3.eth.getTransactionCount(fromAddress);
                 var contractAddress = "0x0E618c94FC648369810e0ae581964E5e631a6d82";
-
+                var  contractAbi = [
+                    {
+                        "constant": true,
+                        "inputs": [],
+                        "name": "totalSupply",
+                        "outputs": [
+                            {
+                                "name": "",
+                                "type": "uint256"
+                            }
+                        ],
+                        "payable": false,
+                        "stateMutability": "view",
+                        "type": "function"
+                    },
+                    {
+                        "constant": true,
+                        "inputs": [
+                            {
+                                "name": "_owner",
+                                "type": "address"
+                            }
+                        ],
+                        "name": "balanceOf",
+                        "outputs": [
+                            {
+                                "name": "balance",
+                                "type": "uint256"
+                            }
+                        ],
+                        "payable": false,
+                        "stateMutability": "view",
+                        "type": "function"
+                    },
+                    {
+                        "constant": false,
+                        "inputs": [
+                            {
+                                "name": "_to",
+                                "type": "address"
+                            },
+                            {
+                                "name": "_value",
+                                "type": "uint256"
+                            }
+                        ],
+                        "name": "transfer",
+                        "outputs": [],
+                        "payable": false,
+                        "stateMutability": "nonpayable",
+                        "type": "function"
+                    },
+                    {
+                        "anonymous": false,
+                        "inputs": [
+                            {
+                                "indexed": true,
+                                "name": "from",
+                                "type": "address"
+                            },
+                            {
+                                "indexed": true,
+                                "name": "to",
+                                "type": "address"
+                            },
+                            {
+                                "indexed": false,
+                                "name": "value",
+                                "type": "uint256"
+                            }
+                        ],
+                        "name": "Transfer",
+                        "type": "event"
+                    }
+                ];
                 //ส่งให้ใคร , จำนวน eth , gasLimit , gasPrice
                 // const txData = {
                 //     to: addressTo,
